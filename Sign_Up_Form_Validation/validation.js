674347ae451111e15123a5a098c9bb0387c2fcf0
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-10 22:40:56 +0500

    Signup form js code added 03/10/2025

--- Sign_Up_Form_Validation/validation.js
+++ Sign_Up_Form_Validation/validation.js
@@ -1,6 +1,11 @@
-
+var pass1;
+var upCase = document.getElementById('uppercase');
+var loCase = document.getElementById('lowercase');
+var specialCh = document.getElementById('special');
+var upCase = document.getElementById('number');
 function validPass1(val1){
     // alert(val1.value); // working properly
+
 }
 function validPass2(val2){
     // alert(val2.value); // this is also working properly
